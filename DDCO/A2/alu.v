48187a394c596773cce4cb041cb93b3ccc03797c
Karthik Ram <92427479+keju27@users.noreply.github.com>
2022-09-30 15:19:21 +0530

    Add files via upload

---
+++ DDCO/A2/alu.v
@@ -0,0 +1,11 @@
+
+// Write code for modules you need here
+
+module alu (input wire [1:0] op, input wire [15:0] i0, i1,
+    output wire [15:0] o, output wire cout);
+
+// Declare wires here
+
+// Instantiate modules here
+
+endmodule
