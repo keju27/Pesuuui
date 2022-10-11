8b6340fa0675fcda3831d1f2c51797476b82739d
Karthik Ram <92427479+keju27@users.noreply.github.com>
2022-10-11 11:17:23 +0530

    Add files via upload
    added lab6

---
+++ DSA/lab6-DSA-boilerplate.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct MinHeap MinHeap;
+struct MinHeap {
+    int* arr;
+    int size;  //size of the heap
+};
+
+
+MinHeap* init_minheap(int size) {
+    MinHeap* minheap = (MinHeap*)calloc(1, sizeof(MinHeap));
+    minheap->size = size;
+    minheap->arr = (int*)calloc(size, sizeof(int));
+    return minheap;
+}
+
+void create_minheap(MinHeap *heap) 
+{   
+    //IMPLEMENT HERE
+
+}
+
+
+
+void print_minheap(MinHeap* heap) {
+    // IMPLEMENT HERE
+}
+
+void free_minheap(MinHeap* heap) {
+    if (!heap)
+        return;
+    free(heap->arr);
+    free(heap);
+}
+
+int main() {
+
+    int capacity; 
+    scanf("%d", &capacity);
+    MinHeap* heap = init_minheap(capacity); //number of elements 
+    int i=0;
+    while(i < capacity){
+        scanf("%d", &heap->arr[i++]);
+
+    }
+    create_minheap(heap);
+    print_minheap(heap);
+    free_minheap(heap);
+    return 0;
+}
