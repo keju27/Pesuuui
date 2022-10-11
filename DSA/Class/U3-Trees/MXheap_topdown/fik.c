55889c90fc3b3e15b4debb02b391883c66d20257
Keju27 <karthikram2703@gmail.com>
2022-10-11 11:15:38 +0530

    random

--- DSA/Class/U3-Trees/MXheap_topdown/fik.c
+++ DSA/Class/U3-Trees/MXheap_topdown/fik.c
@@ -1,26 +1,83 @@
 #include<stdio.h>
 #define MAX 50
 
-int topDownHeap(int h[],int n);
-int n;
-
-printf("enter the elements/n");
-
-int topDownHeap(int h[], int n)
+void topDownHeap(int h[],int n);
+int main()
 {
-    int c,p,key;
-    for (int i=1;i<n;i++)
-    {
-        c=i;
-        p=(c-1)/2;
-        key=h[c];
-
-        while(c>0 && key >h[p])
-        {
-            h[c]=h[p];
-            c=p;
-            p=(c-1)/2;
-        }
-        h[c]=key;
-    }
+	int h[MAX];
+	int n;
+	
+	printf("Enter the no. of elements\n");
+	scanf("%d",&n);
+	
+	printf("Enter the elements\n");
+	for(int i=0;i<n;i++)
+		scanf("%d",&h[i]);
+		
+	topDownHeap(h,n);
+    printf("%c","|");
+	for(int i=0;i<n;i++)
+		printf("%d ",h[i]);
 }
+
+void topDownHeap(int h[],int n)
+{
+	int c,p,key;
+	
+	for(int i=1;i<n;i++)
+	{
+		c=i;
+		p=(c-1)/2;
+		key=h[c];
+		
+		while(c>0 && key > h[p])
+		{
+			h[c]=h[p];
+			c=p;
+			p=(c-1)/2;
+		}
+		h[c]=key;
+	}
+}
+/* 
+#include<stdio.h>
+#define MAX 50
+
+void topDownHeap(int h[],int n);
+int main()
+{
+	int h[MAX];
+	int n;
+	
+	printf("Enter the no. of elements\n");
+	scanf("%d",&n);
+	
+	printf("Enter the elements\n");
+	topDownHeap(h,n);
+
+	for(int i=0;i<n;i++)
+		printf("%d ",h[i]);
+}
+
+void topDownHeap(int h[],int n)
+{
+	int c,p,key;
+	
+	scanf("%d",&h[0]);
+	
+	for(int i=1;i<n;i++)
+	{
+		scanf("%d",&h[i]);
+		c=i;
+		p=(c-1)/2;
+		key=h[c];
+		
+		while(c>0 && key > h[p])
+		{
+			h[c]=h[p];
+			c=p;
+			p=(c-1)/2;
+		}
+		h[c]=key;
+	}
+} */
--- DSA/Class/U3-Trees/bst.c
+++ DSA/Class/U3-Trees/bst.c
@@ -1,6 +1,27 @@
+#include<stdlib.h>
 #include<stdio.h>
-#include"bst.h"
+typedef struct node
+{
+	int info;
+	struct node *left;
+	struct node *right;
+}NODE;
 
+typedef struct tree
+{
+	NODE *root;
+}TREE;
+
+void initTree(TREE *pt);
+void insertNode(TREE *pt,int ele);
+void inorderTraversal(TREE *pt);
+void preorderTraversal(TREE *pt);
+void postorderTraversal(TREE *pt);
+void destroyTree(TREE *pt);
+void insertRec(TREE *pt,int ele);
+NODE* searchIterative(TREE *pt,int ele);
+NODE* searchRecursive(TREE *pt,int ele);
+void deleteNode(TREE *pt,int ele);
 int main()
 {
 	TREE tobj;
@@ -54,4 +75,193 @@ int main()
 		}
 	}while(choice<9);
 	destroyTree(&tobj);
+}
+void initTree(TREE *pt)
+{
+	pt->root=NULL;
+}
+
+NODE* createNode(int ele)
+{
+	NODE *newNode=malloc(sizeof(NODE));
+	
+	newNode->info=ele;
+	newNode->left=NULL;
+	newNode->right=NULL;
+	
+	return newNode;
+}
+
+void insertNode(TREE *pt,int ele)
+{
+	NODE *p=pt->root;
+	NODE *q=NULL;
+	
+	NODE *newNode=createNode(ele);
+	
+	if(pt->root==NULL)
+		pt->root=newNode;
+	else
+	{
+		while(p!=NULL)
+		{
+			q=p;
+		
+			if(newNode->info < p->info)
+				p=p->left;
+			else
+				p=p->right;
+		}
+		if(newNode->info < q->info)
+			q->left=newNode;
+		else
+			q->right=newNode;
+	}	
+}
+
+NODE* insertNodeRec(NODE* p,NODE* newNode)
+{
+	if(p==NULL)
+		return newNode;
+	else if(newNode->info < p->info)
+		p->left=insertNodeRec(p->left,newNode);
+	else
+		p->right=insertNodeRec(p->right,newNode);
+	
+	return p;
+}
+void insertRec(TREE *pt,int ele)
+{
+	NODE *newNode=createNode(ele);
+	pt->root=insertNodeRec(pt->root,newNode);
+}
+
+void inorder(NODE *p)
+{
+	if(p!=NULL)
+	{
+		inorder(p->left);
+		printf("%d ",p->info);
+		inorder(p->right);
+	}
+}
+void inorderTraversal(TREE *pt)
+{
+	inorder(pt->root);
+}
+void preorder(NODE *p)
+{
+	if(p!=NULL)
+	{
+		printf("%d ",p->info);
+		preorder(p->left);
+		preorder(p->right);
+	}
+}
+void preorderTraversal(TREE *pt)
+{
+	preorder(pt->root);
+}
+void postorder(NODE *p)
+{
+	if(p!=NULL)
+	{
+		postorder(p->left);
+		postorder(p->right);
+		printf("%d ",p->info);
+	}
+}
+void postorderTraversal(TREE *pt)
+{
+	postorder(pt->root);
+}
+void destroyNode(NODE *p)
+{
+	if(p!=NULL)
+	{
+		destroyNode(p->left);
+		destroyNode(p->right);
+		free(p);
+	}
+}
+void destroyTree(TREE *pt)
+{
+	if(pt->root!=NULL)
+		destroyNode(pt->root);
+	
+	pt->root=NULL;
+}
+
+NODE* searchIterative(TREE *pt,int ele)
+{
+	NODE *p=pt->root;
+	
+	while(p!=NULL)
+	{
+		if(ele == p->info)
+			return p;
+		else if(ele < p->info)
+			p=p->left;
+		else
+			p=p->right;
+	}
+	return NULL;		//return p;
+}
+
+NODE* searchNode(NODE *p,int ele)
+{
+	if(p==NULL)
+		return p;
+	else if(ele == p->info)
+		return p;
+	else if(ele < p->info)
+		return searchNode(p->left,ele);
+	else 
+		return searchNode(p->right,ele);
+}
+NODE* searchRecursive(TREE *pt,int ele)
+{
+	return searchNode(pt->root,ele);
+}
+NODE* delNode(NODE *p,int ele)
+{
+	NODE *q;
+	if(p==NULL)
+		return p;
+	else if(ele<p->info)
+		p->left=delNode(p->left,ele);
+	else if(ele > p->info)
+		p->right=delNode(p->right,ele);
+	else
+	{
+		if(p->left==NULL)		//Single right child or leaf node
+		{
+			q=p->right;
+			free(p);
+			return q;
+		}
+		else if(p->right==NULL)			//single left child
+		{
+			q=p->left;
+			free(p);
+			return q;
+		}
+		else		//2 children: replace by inorder successor
+		{
+			q=p->right;
+			while(q->left!=NULL)
+				q=q->left;
+			
+			p->info=q->info;
+			p->right=delNode(p->right,q->info);
+		}
+	}
+	return p;
+}
+void deleteNode(TREE *pt,int ele)
+{
+	if(pt->root!=NULL)
+	{
+		pt->root=delNode(pt->root,ele);
+	}
 }
