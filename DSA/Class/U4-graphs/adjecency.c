14e24ec0905f946a9f6158454907a398e835d7bd
Keju27 <karthikram2703@gmail.com>
2022-10-17 11:08:09 +0530

    changes

---
+++ DSA/Class/U4-graphs/adjecency.c
@@ -0,0 +1,27 @@
+#define MAX 10
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+    int info;
+    struct node *next;
+}NODE;
+
+void initGraph(NODE *V[MAX]);
+void createGraph(NODE *V[MAX]);
+int main()
+{
+    NODE *
+    createGraph
+}
+
+void initGraph(NODE *V[MAX])
+{
+    for 
+
+}
+
+
+
+void            
